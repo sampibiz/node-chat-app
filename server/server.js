68009464c3ab9cbea7a0032bd7b79f6d508d1947
Boaz Shabon <boaz@sampi.co.il>
2018-09-06 19:04:07 +0300

    Add socketIO to server and listening to some events

--- server/server.js
+++ server/server.js
@@ -1,14 +1,26 @@
 const express = require( 'express' );
+const http = require('http');
+const socketIO = require('socket.io');
 const path = require('path');
+
 
 const publicPath = path.join( __dirname, '../public' );
 const port = process.env.PORT || 3000;
 var app = express();
+var server = http.createServer( app );
+var io = socketIO( server );
 app.use( express.static( publicPath ) );
 
+io.on( 'connection', ( socket ) => {
+    console.log( 'New user connection' );
 
-app.listen( port, () => {
+    socket.on( 'disconnect', ( socket ) => {
+        console.log( 'User Disconnect' );
+    } );
+} );
+
+
+
+server.listen( port, () => {
     console.log( `Server is running on port ${port}` );
 });
-
-
