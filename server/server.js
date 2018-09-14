b53c2af8e113a4d6660f091d696dd9567aa16c7f
Boaz Shabon <boaz@sampi.co.il>
2018-09-14 16:07:36 +0300

    Create generateMessage utility

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const http = require('http');
 const socketIO = require('socket.io');
 const path = require('path');
 
+var {generateMessage} = require( './utils/message' );
 
 const publicPath = path.join( __dirname, '../public' );
 const port = process.env.PORT || 3000;
@@ -14,32 +15,17 @@ app.use( express.static( publicPath ) );
 io.on( 'connection', ( socket ) => {
     console.log( 'New user connection' );
 
-    socket.emit( 'newMessage', {
-        from: 'Admin',
-        text: 'Welcome to our chat',
-        createdAt: new Date().getTime()
-    } );
+    socket.emit( 'newMessage', generateMessage( 'Admin', 'Welcome to our chat' ) );
 
-    socket.broadcast.emit( 'newMessage', {
-        from: 'Admin',
-        text: 'New user joined',
-        createdAt: new Date().getTime()
-    } );
 
-    socket.emit( 'newMessage', {
-        from: 'Michael',
-        text: 'New message',
-        createdAt: new Date().getTime()
-    } );
+    socket.broadcast.emit( 'newMessage', generateMessage( 'Admin', 'New user joined' ) );
+
+    socket.emit( 'newMessage', generateMessage( 'Michael', 'New message' ) );
 
     socket.on( 'createMessage', ( message ) => {
         console.log( 'Create message:', message );
 
-        io.emit( 'newMessage', {
-            from: message.from,
-            text: message.text,
-            createdAt: new Date().getTime()
-        } );
+        io.emit( 'newMessage', generateMessage( message.from, message.text ) );
     });
 
     socket.on( 'disconnect', ( socket ) => {
---
+++ server/utils/message.test.js
@@ -0,0 +1,13 @@
+var expect = require( 'expect' );
+var {generateMessage} = require( './message' );
+
+describe( 'generateMessage', () => {
+    it( 'Should generate the correct message object', () =>{
+        var from = 'Danni';
+        var text = 'Test message';
+        var message = generateMessage( from, text );
+
+        expect( typeof message.createdAt ).toBe( 'number' );
+        expect( message ).toMatchObject( { from, text } );
+    } )
+});
